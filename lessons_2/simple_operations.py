6912d15fa4889eb28db2add538b42e14ea616f3b
Dim-Nike <73593239+Dim-Nike@users.noreply.github.com>
2021-10-29 14:56:38 +0300

    Add files via upload
    added py_start - ten lessons

---
+++ lessons_2/simple_operations.py
@@ -0,0 +1,14 @@
+#------TRIGGER__PY_START------
+
+a = float(input("Введите 1 значение: "))
+s = input("Введите 2 значение: ")
+
+x = float(s)
+
+print("Сумма:", a + x)
+minus = a - x
+print("Разница:", minus)
+mult = a * x
+print("Умножение:", mult)
+delenie = a / x
+print("Деление:", delenie)
